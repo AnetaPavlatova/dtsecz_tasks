ab1486a7322f465287c4b5030e1d7465ff7d86d3
Aneta Jirkova <anetajirkov@gmail.com>
2023-07-16 21:38:15 +0200

    Update files

--- sql_task_1/sql_jobtask1_done.sql
+++ sql_task_1/sql_jobtask1_done.sql
@@ -1,9 +1,111 @@
--- TABLE
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+--TASK 1--
+--Write query which will show those customers with contacts and with orders (all columns)
+
+
+SELECT cu.*,
+	o.order_id,
+    o.item,
+    o.order_value,
+    o.order_currency,
+    o.order_date,
+    co.address,
+    co.city,
+    co.phone_number,
+    co.email
+FROM customers AS cu
+INNER JOIN orders as o
+ON cu.customer_id = o.customer_id
+INNER JOIN contacts as co
+ON cu.customer_id = co.customer_id
+ORDER BY customer_id;
+
+
+--TASK 2--
+-- There is  suspision that some orders were wrongly inserted more times. Check if there are any duplicated orders. If so, return unique duplicates with the following details:
+-- first name, last name, email, order id and item
+
+-- Checking if there are any duplicate orders:
+
+SELECT order_id, item, COUNT(order_id) as amount_orders
+FROM orders
+GROUP BY order_id, item
+HAVING amount_orders > 1;
+
+-- YES, there are 2 duplicate orders. Now, I will find more details about the duplicate orders:
+
+SELECT cu.first_name,
+	cu.last_name,
+    co.email,
+    o.order_id,
+    o.item,
+    COUNT(o.order_id) AS num_orders
+FROM orders AS o
+LEFT JOIN customers AS cu
+ON cu.customer_id = o.customer_id
+JOIN contacts as co
+ON cu.customer_id = co.customer_id
+GROUP BY cu.first_name, cu.last_name, co.email, o.order_id
+HAVING num_orders > 1;
+
+
+--TASK 3--
+-- As you found out, there are some duplicated order which are incorrect, adjust query so it does following:
+-- Show first name, last name, email, order id and item
+-- Does not show duplicates
+-- Order result by customer last name
+
+SELECT DISTINCT cu.first_name,
+	cu.last_name,
+    co.email,
+    o.order_id,
+    o.item
+FROM orders AS o
+JOIN customers AS cu
+ON cu.customer_id = o.customer_id
+JOIN contacts as co
+ON cu.customer_id = co.customer_id
+ORDER BY cu.last_name;
+
+
+--TASK 4--
+--Our company distinguishes orders to sizes by value like so:
+--order with value less or equal to 25 euro is marked as SMALL
+--order with value more than 25 euro but less or equal to 100 euro is marked as MEDIUM
+--order with value more than 100 euro is marked as BIG
+--Write query which shows only three columns: full_name (first and last name divided by space), order_id and order_size
+--Make sure the duplicated values are not shown
+--Sort by order ID (ascending)
+
+SELECT DISTINCT cu.first_name || ' ' || cu.last_name AS full_name,
+	o.order_id,
+    CASE WHEN o.order_value > 100 THEN 'BIG'
+    WHEN o.order_value > 25 THEN 'MEDIUM'
+    WHEN o.order_value <= 25 THEN 'SMALL'
+    ELSE '00-error'
+    END AS order_size
+FROM orders AS o
+JOIN customers AS cu
+ON cu.customer_id = o.customer_id
+ORDER BY order_id;
+
+--TASK 5--
+-- Show all items from orders table which containt in their name 'ea' or start with 'Key'
+
+SELECT item
+FROM orders
+WHERE item LIKE '%ea%' OR item LIKE 'Key%';
  
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
+--TASK 6--
+-- Please find out if some customer was referred by already existing customer
+-- Return results in following format "Customer Last name Customer First name" "Last name First name of customer who recomended the new customer"
+-- Sort/Order by customer last name (ascending)
+
+SELECT
+  c1.last_name || ' ' || c1.first_name AS "customer",
+  c2.last_name || ' ' || c2.first_name AS "recommended_by"
+FROM
+  customers c1
+JOIN
+  customers c2 ON c1.referred_by_id = c2.customer_id
+ORDER BY
+  c1.last_name ASC;
--- sql_task_2/sql_optimize_done.sql
+++ sql_task_2/sql_optimize_done.sql
@@ -1,9 +1,53 @@
--- TABLE
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
- 
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
+--- Point to parts that could be optimized
+--- Feel free to comment any row that you think could be optimize/adjusted in some way!
+--- The following query is from SAP HANA but applies to any DB
+--- Do not worry if the tables/columns are not familiar to you 
+----   -> you do not need to interpret the result (in fact the query does not reflect actual DB content)
+SELECT 
+	RSEG.EBELN,
+	RSEG.EBELP,
+    RSEG.BELNR,
+    RSEG.AUGBL AS AUGBL_W,
+    LPAD(EKPO.BSART,6,0) as BSART, -- EDIT: LPAD(EKPO.BSART,6,'0') --editing '0'
+	BKPF.GJAHR,
+	BSEG.BUKRS,
+	BSEG.BUZEI,
+	BSEG.BSCHL,
+	BSEG.SHKZG,
+    CASE WHEN BSEG.SHKZG = 'H' THEN (-1) * BSEG.DMBTR ELSE BSEG.DMBTR END AS DMBTR,
+    COALESCE(BSEG.AUFNR, 'Kein SM-A Zuordnung') AS AUFNR,
+    COALESCE(LFA1.LAND1, 'Andere') AS LAND1, 
+    LFA1.LIFNR,
+    LFA1.ZSYSNAME,
+    BKPF.BLART as BLART,
+    BKPF.BUDAT as BUDAT,
+    BKPF.CPUDT as CPUDT
+FROM "DTAG_DEV_CSBI_CELONIS_DATA"."dtag.dev.csbi.celonis.data.elog::V_RSEG" AS RSEG
+LEFT JOIN "DTAG_DEV_CSBI_CELONIS_WORK"."dtag.dev.csbi.celonis.app.p2p_elog::__P2P_REF_CASES" AS EKPO ON 1=1 --REMOVE ON 1=1
+    AND RSEG.ZSYSNAME = EKPO.SOURCE_SYSTEM --REPLACE 'AND' WITH ON (connected to the change in previous line)
+    AND RSEG.MANDT = EKPO.MANDT
+    AND RSEG.EBELN || RSEG.EBELP = EKPO.EBELN || EKPO.EBELP 
+INNER JOIN "DTAG_DEV_CSBI_CELONIS_DATA"."dtag.dev.csbi.celonis.data.elog::V_BKPF" AS BKPF ON 1=1  --REMOVE ON 1=1 AND AGAIN REPLACE "AND" WITH "ON"
+    AND BKPF.AWKEY = RSEG.AWKEY
+    AND RSEG.ZSYSNAME = BKPF.ZSYSNAME
+    AND RSEG.MANDT in ('200')
+INNER JOIN "DTAG_DEV_CSBI_CELONIS_DATA"."dtag.dev.csbi.celonis.data.elog::V_BSEG" AS BSEG ON 1=1  --REMOVE ON 1=1 AND AGAIN REPLACE "AND" WITH "ON"
+    AND DATS_IS_VALID(BSEG.ZFBDT) = 1 -- REMOVE FUNCTION HERE AND PUT IT IN THE WHERE CLAUSE INSTEAD
+    AND BSEG.KOART = 'K'
+    AND CAST(BSEG.GJAHR AS INT) = 2020
+    AND BKPF.ZSYSNAME = BSEG.ZSYSNAME
+    AND BKPF.MANDT = BSEG.MANDT
+    AND BKPF.BUKRS = BSEG.BUKRS
+    AND BKPF.GJAHR = BSEG.GJAHR
+    AND BKPF.BELNR = BSEG.BELNR
+    AND BSEG.DMBTR*-1 >= 0
+INNER JOIN (SELECT * FROM "DTAG_DEV_CSBI_CELONIS_DATA"."dtag.dev.csbi.celonis.data.elog::V_LFA1" AS TEMP
+            WHERE TEMP.LIFNR > '020000000') AS LFA1 ON 1=1 --REMOVE THE SUBSELECT
+--INNER JOIN "DTAG_DEV_CSBI_CELONIS_DATA"."dtag.dev.csbi.celonis.data.elog::V_LFA1" AS LFA1 -- ADD INSTEAD OF THE SUBSELECT AND ADD WHERE CLAUSE
+    AND BSEG.ZSYSNAME = LFA1.ZSYSNAME --ON INSTEAD OF AND
+    AND BSEG.LIFNR=LFA1.LIFNR
+    AND BSEG.MANDT=LFA1.MANDT
+    AND LFA1.LAND1 in ('DE','SK')
+--WHERE 
+--	LFA1.LIFNR > '020000000' -- ADD HERE BECAUSE WE REMOVED THE SUBSELECT
+;
